1ac83a33d6c74b808415478a2449f8479e0333b7
Prasun Chakraborty <prasun.c@bridgera.com>
2019-10-23 19:10:07 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Part = (params) => {
+    console.log(params)
+    return (
+        <p>
+            {params.part.name} {params.part.exercises}
+        </p>
+    )
+}
+const Header = (params) => {
+    return (
+        <h1>{params.course}</h1>
+    )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
+const Content = (params) => {
+    return (
+        <div>
+            <Part part={params.parts[0]} />
+            <Part part={params.parts[1]} />
+            <Part part={params.parts[2]} />
+        </div>
+    )
+
+}
+const Total = (params) => {
+    console.log(params.parts);
+    return (
+        <p>Number of exercises {params.parts[0].exercises + params.parts[1].exercises + params.parts[2].exercises}</p>
+    )
+
+
+}
+const App = () => {
+    const course = {
+        name: ' ',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
