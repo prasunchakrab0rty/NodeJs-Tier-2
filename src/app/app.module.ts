17075ee3d1d4eb483326a3978ddde0772e64e91f
Prasun Chakraborty <lordprasun@gmail.com>
2020-09-01 09:37:25 +0530

    Add name Filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list/product-list.component";
-import { SpecialToSpacePipe } from './shared/pipes/special-to-space.pipe';
+import { SpecialToSpacePipe } from "./shared/pipes/special-to-space.pipe";
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent, SpecialToSpacePipe],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
